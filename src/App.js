01fa1895a937c2963d09dbc2f43eba902bd577d1
Yurii Chajka <yuriichajka@gmail.com>
2021-10-22 21:45:15 +0300

    add modal, loader, axios, custom hooks

--- src/App.js
+++ src/App.js
@@ -1,54 +1,89 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'},
-    ])
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-    const [filter, setFilter] = useState({
-        sort: '', query: ''
+    let pagesArray = getPagesArray(totalPages)
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
     })
 
-    const sortedPosts = useMemo(() => {
-        console.log('wowo')
-        if (filter.sort) {
-            return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts()
+    }
+
   return (
     <div className="App">
-        <PostForm create={createPost} />
+        <MyButton onClick={fetchPosts}>Get Posts</MyButton>
+        <MyButton style={{ marginTop: '25px' }} onClick={() => setModal(true)}>
+            Create
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost} />
+        </MyModal>
         <hr style={{ margin: '15px 0' }} />
         <PostFilter
             filter={filter}
             setFilter={setFilter}
         />
-        <PostList
-            posts={sortedAndSearchedPosts}
-            title={'Some title'}
-            remove={removePost}
-        />
+        {postError &&
+            <h1>Some error ${postError}</h1>
+        }
+        {isPostsLoading
+            ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
+            : <PostList
+                posts={sortedAndSearchedPosts}
+                title={'Some title'}
+                remove={removePost}
+            />
+        }
+        <div className='page__wrapper'>
+            {pagesArray.map(p =>
+                <span
+                    onClick={() => changePage(p)}
+                    key={p}
+                    className={page === p ? 'page page__current' : 'page'}
+                >
+                    {p}
+                </span>
+            )}
+        </div>
     </div>
   );
 }
